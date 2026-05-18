698809e3a8adefe5186766d949cb36f8346b51d1
mayor <ru93ben@icloud.com>
2026-05-17 20:05:47 -0700

    feat: add Scene composition (Canvas + ScrollControls + all children)

---
+++ src/components/canvas/Scene.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { Canvas } from '@react-three/fiber';
+import { ScrollControls } from '@react-three/drei';
+import { Suspense } from 'react';
+import { Camera } from './Camera';
+import { DustField } from './DustField';
+import { Supernova } from './Supernova';
+import { TopologyCloud } from './TopologyCloud';
+import { PostProcessing } from './PostProcessing';
+import { HeroOverlay } from '../overlay/HeroOverlay';
+import { AboutOverlay } from '../overlay/AboutOverlay';
+import { NodeOverlay } from '../overlay/NodeOverlay';
+import { ProjectsOverlay } from '../overlay/ProjectsOverlay';
+import { TechOverlay } from '../overlay/TechOverlay';
+import { ContactOverlay } from '../overlay/ContactOverlay';
+import { PortalTransition } from '../overlay/PortalTransition';
+
+export function Scene() {
+  return (
+    <>
+      <Canvas
+        camera={{ fov: 60, near: 0.1, far: 200, position: [0, 0, 2] }}
+        style={{ position: 'fixed', inset: 0 }}
+        gl={{ antialias: true, alpha: false }}
+        dpr={[1, 1.5]}
+        role="img"
+        aria-label="Interactive 3D visualization of the Atlax tunnel system architecture"
+      >
+        <color attach="background" args={['#050508']} />
+        <Suspense fallback={null}>
+          <ScrollControls pages={8} damping={0.15}>
+            <Camera />
+            <DustField />
+            <Supernova />
+            <TopologyCloud />
+            <HeroOverlay />
+            <AboutOverlay />
+            <NodeOverlay />
+            <ProjectsOverlay />
+            <TechOverlay />
+            <ContactOverlay />
+          </ScrollControls>
+          <PostProcessing />
+        </Suspense>
+      </Canvas>
+      <PortalTransition />
+    </>
+  );
+}
